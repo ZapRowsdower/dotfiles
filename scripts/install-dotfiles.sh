86aed209b5bb711dfe9067cbf3c4e7fb150a6062
57988 <jonathan_ely@navyfederal.org>
2022-07-11 09:01:21 -0400

    chore: commit save point

---
+++ scripts/install-dotfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -s ~/dotfiles/bash/.profile ~/.profile
+ln -s ~/dotfiles/bash/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/dotfiles/zsh/.zprofile ~/.zprofile
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
