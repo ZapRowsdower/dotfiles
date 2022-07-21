11f17b127fc22d359462a39d6bf4c891baefb607
Jonathan Ely <ely.jonathan@gmail.com>
2022-07-21 08:25:32 -0400

    Moves, parses programs in shared Inventory file

--- scripts/install-macOS.sh
+++ scripts/install-macOS.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
-
-if ! [- x "$(command -v brew)" ]; then
+source ./iniget.sh
+if ! [ -x "$(command -v brew)" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 function install {
     which $1 &>/dev/null
-
+    
     if [ $? -ne 0 ]; then
         echo "Installing: ${1}..."
-        brew install $1
+        arch -arm64 brew install $1
     else
         echo "Already installed: ${1}"
     fi
 }
 
-# Basics
-# https://github.com/BurntSushi/ripgrep
-install ripgrep
-install brave-browser
-install curl
-install git
-install htop
-install tree
-install vim
-install wget
-install neofetch
-# Great WM: https://rectangleapp.com/
-install rectangle
+commonProgramsToInstall=$(iniget ./programs/Inventory common)
+
+for program in $commonProgramsToInstall; do
+    install "$program"
+done
+
+macProgramsToInstall=$(iniget ./programs/Inventory macos)
+
+for program in $macProgramsToInstall; do
+    install "$program"
+done
